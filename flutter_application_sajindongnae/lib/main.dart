8735de241144e8c7d20e76a829ed76bba8898ec6
HGM0223 <annie5648@naver.com>
2025-05-27 17:08:22 +0900

    update
    1. 스플래시 화면 만듦
    - 처음에 만들었던 splach.dart없애고 새로 만듦
    - main.dart 살짝 수정
    - 관련 패키지 설치
    - 로고 이름 변경 assets/images/Logo.png -> assets/icons/app_icon.png

--- flutter_application_sajindongnae/lib/main.dart
+++ flutter_application_sajindongnae/lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-import 'splash_screen.dart'; // 불러오기
 
 void main() {
   runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: SplashScreen(),  // 시작화면 설정
+    home: MainPage(), // 이걸 반드시 넣어줘야 화면이 보임
   ));
 }
 
