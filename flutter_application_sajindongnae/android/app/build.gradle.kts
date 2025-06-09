9005fc6c5db6e35c02070ab934121a6dda1ed414
MinChaeY <127286485+MinChaeY@users.noreply.github.com>
2025-06-10 01:26:43 +0900

    게시글 작성 기능 및 Firebase Firestore 연동
    - Firebase Firestore 연동을 위한 SDK 및 초기화 설정 완료
    - post_model.dart 구조 개선 (fromMap → fromDocument 방식)
    - post_service.dart 생성: 게시글 업로드 메서드 구현
    - Android build.gradle.kts 설정 수정 (minSdk 23, 플러그인 적용)

--- flutter_application_sajindongnae/android/app/build.gradle.kts
+++ flutter_application_sajindongnae/android/app/build.gradle.kts
@@ -3,6 +3,9 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    // Add the Google services Gradle plugin
+    id("com.google.gms.google-services")
+
 }
 
 android {
@@ -24,7 +27,8 @@ android {
         applicationId = "com.example.flutter_application_sajindongnae"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        // minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -41,4 +45,14 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+  // Import the Firebase BoM
+  implementation(platform("com.google.firebase:firebase-bom:33.15.0"))
+
+
+  // TODO: Add the dependencies for Firebase products you want to use
+  // When using the BoM, don't specify versions in Firebase dependencies
+  // https://firebase.google.com/docs/android/setup#available-libraries
 }
