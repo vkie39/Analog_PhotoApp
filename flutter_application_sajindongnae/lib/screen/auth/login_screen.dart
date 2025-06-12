99a49f4c8b8ee2249f999cb4a7561b0978e30d05
vkie39 <107172953+vkie39@users.noreply.github.com>
2025-06-12 20:07:57 +0900

    Fix missing newline

--- flutter_application_sajindongnae/lib/screen/auth/login_screen.dart
+++ flutter_application_sajindongnae/lib/screen/auth/login_screen.dart
@@ -1,4 +1,3 @@
-/*
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -73,4 +72,3 @@ class _LoginScreenState extends State<LoginScreen> {
     );
   }
 }
-*/
