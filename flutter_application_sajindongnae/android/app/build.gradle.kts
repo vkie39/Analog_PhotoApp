7b856f8578a4842eeb65d1720c5b0920877387f5
HGM0223 <annie5648@naver.com>
2025-09-24 17:56:42 +0900

    Fix: xml 파일 수정해서 로그인 화면까지 진입 성공

--- flutter_application_sajindongnae/android/app/build.gradle.kts
+++ flutter_application_sajindongnae/android/app/build.gradle.kts
@@ -1,16 +1,10 @@
 plugins {
     id("com.android.application")
-<<<<<<< HEAD
-    id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
-    id("dev.flutter.flutter-gradle-plugin")
-    // Add the Google services Gradle plugin 
-=======
-    id("kotlin-android")  // ✅ 이게 결국 org.jetbrains.kotlin.android로 매핑됨
+
+    id("kotlin-android")  
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
     // Add the Google services Gradle plugin
->>>>>>> origin/main
     id("com.google.gms.google-services")
 
 }
@@ -21,40 +15,23 @@ android {
     ndkVersion = "27.0.12077973"
 
     compileOptions {
-<<<<<<< HEAD
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
-=======
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
         jvmTarget = "17"
->>>>>>> origin/main
     }
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-<<<<<<< HEAD
         applicationId = "com.example.flutter_application_sajindongnae"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         // minSdk = flutter.minSdkVersion
-        minSdk = 24
+        minSdk = 24                      
         targetSdk = 35
-=======
-        applicationId = "com.example.flutter_application_sajindongnae" 
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        // minSdk = flutter.minSdkVersion
-        minSdk = 23
-        targetSdk = flutter.targetSdkVersion
->>>>>>> origin/main
+
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
@@ -74,17 +51,16 @@ flutter {
 
 dependencies {
   // Import the Firebase BoM
-<<<<<<< HEAD
-  implementation(platform("com.google.firebase:firebase-bom:33.15.0")) 
+
 
 
   // TODO: Add the dependencies for Firebase products you want to use
   // When using the BoM, don't specify versions in Firebase dependencies
   // https://firebase.google.com/docs/android/setup#available-libraries
-=======
+
   implementation(platform("com.google.firebase:firebase-bom:34.2.0"))
 
 
-    implementation("com.google.firebase:firebase-auth")
->>>>>>> origin/main
+  implementation("com.google.firebase:firebase-auth")
+
 }
